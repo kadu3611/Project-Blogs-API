f784647780ecdc28697d16592fab822a1a5d0e3a
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2022-08-25 19:03:15 -0300

    Feito req 2

---
+++ src/database/models/user.js
@@ -0,0 +1,22 @@
+'use strict';
+const Users = (sequelize, DataTypes) => {
+  const Users = sequelize.define("User", {
+  id: {type: DataTypes.INTEGER, primaryKey: true},
+  displayName: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+  image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  });
+
+  Users.associate = (models) => {
+    Users.hasMany(models.User, { foreignKey: 'id', as: 'Users' });
+  };
+
+  return Users;
+    };
+
+
+module.exports = Users;
